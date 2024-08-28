c1439d6345f82f260095042533d4bca73a64ac33
Leonardo-Broinizi <leonardobroinizidefarias@gmail.com>
2024-08-28 14:46:36 -0300

    .

---
+++ aula13_iteracoes_lacos_repeticoes/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c ++
+}
+
+do {
+    console.log(`Passo ${c}`)
+    c --
+} while (c > 0)
+
+
+
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c ++
+}
+
+do {
+    console.log(`Passo ${c}`)
+    c --
+} while (c > 0)
+  */
