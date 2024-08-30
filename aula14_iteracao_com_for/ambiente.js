dd7387760e33679797404c0f3312f4db248f9c48
Leonardo-Broinizi <leonardobroinizidefarias@gmail.com>
2024-08-30 14:01:19 -0300

    Estrutura 'for' em JavaScritp (sintaxe diferente da usada pelo Python) e uso do debug pela primeira vez.

---
+++ aula14_iteracao_com_for/ambiente.js
@@ -0,0 +1,11 @@
+console.log('Vai começar...')
+for (var c =  1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('Fim!')
+
+
+/* Nessa aula usei o 'debug' pela primeira vez. Ele se encontra no ícone com um inseto e um play, 
+e serve para depurarmos parte do código, ou seja, analisarmos com minúcia o seu funcionamento,
+podendo vê-lo funcionando lentamente, ou pausadamente, e descatar determinada variável. 
+É parecido com o que eu fazia no visualg nas aulas do professor Guanabara de Lógica da Programação. */
