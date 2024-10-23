fd395788992cbead95a1e41967a6bfb19044d9a6
Leonardo-Broinizi <leonardobroinizidefarias@gmail.com>
2024-10-23 20:33:28 -0300

    Fim da primeira parte guiada.
    Aqui o professor passou como exercício a realização das próximas section's sem sua guia. Vou tentar, mas já está salva aqui a parte guiada do código caso algo dê errado.

--- desafios/d012_preco_mudou/script.js
+++ desafios/d012_preco_mudou/script.js
@@ -1,4 +1,25 @@
+function formatarMoeda(valor) {
+    return parseFloat(valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+}
+
 function troca_preco() {
-    // let p_ant = prompt('Qual era o preço anterior do produto?')
-    alert('ola')
+    let p_ant_str = prompt('Qual era o preço anterior do produto?')
+    let p_ant_int = parseInt(p_ant_str)
+    p_ant_str = formatarMoeda(p_ant_int)
+
+    let p_atu_str = prompt('Qual é o preço atual do produto?')
+    
+    let p_atu_int = parseInt(p_atu_str)
+    p_atu_str = formatarMoeda(p_atu_str)
+
+    let entrada = document.getElementById('entrada')
+
+    entrada.innerHTML = `<h2>Analisando os valores informados</h2><br>`
+    if(p_ant_int == p_atu_int) {
+        entrada.innerHTML += `<p>O valor do produto não teve alteração.</p>`
+    } else if(p_ant_int > p_atu_int) {
+    entrada.innerHTML += `<p>O produto custava ${p_ant_str} e agora custa ${p_atu_str}.</p><br><p>Hoje o produto está mais barato.</p>`
+    } else if(p_ant_int < p_atu_int) {
+        entrada.innerHTML += `<p>O produto custava ${p_ant_str} e agora custa ${p_atu_str}.</p><br><p>Hoje o produto está mais caro.</p>`
+    }
 }
